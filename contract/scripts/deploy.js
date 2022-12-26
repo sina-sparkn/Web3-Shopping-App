10219a5e0f1a918ba7b5d04d3b0a755dfabfffdf
ssparknt <sina.hakim.red@gmail.com>
2022-12-27 00:27:18 +0200

    changed the deploy.js file to deploy all file at once

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -1,18 +1,30 @@
 const hre = require("hardhat");
 
 async function main() {
-  const rewardFactory = await hre.ethers.getContractFactory("rewardOne");
-  const rewardContract = await rewardFactory.deploy();
-  await rewardContract.deployed();
+  const rewardFactory1 = await hre.ethers.getContractFactory("rewardOne");
+  const rewardContract1 = await rewardFactory1.deploy();
+  await rewardContract1.deployed();
+  console.log(`contract address 1 : ${rewardContract1.address}`);
 
-  console.log(`contract address : ${rewardContract.address}`);
+  const rewardFactory2 = await hre.ethers.getContractFactory("rewardTwo");
+  const rewardContract2 = await rewardFactory2.deploy();
+  await rewardContract2.deployed();
+  console.log(`contract address 2 : ${rewardContract2.address}`);
 
-  const mintOne = await rewardContract.mintReward1();
-  await mintOne.wait();
-  const mintTwo = await rewardContract.mintReward2();
-  await mintTwo.wait();
-  const mintThree = await rewardContract.mintReward3();
-  await mintThree.wait();
+  const rewardFactory3 = await hre.ethers.getContractFactory("rewardThree");
+  const rewardContract3 = await rewardFactory3.deploy();
+  await rewardContract3.deployed();
+  console.log(`contract address : ${rewardContract3.address}`);
+
+  const rewardFactory4 = await hre.ethers.getContractFactory("rewardFour");
+  const rewardContract4 = await rewardFactory4.deploy();
+  await rewardContract4.deployed();
+  console.log(`contract address : ${rewardContract4.address}`);
+
+  const rewardFactory5 = await hre.ethers.getContractFactory("rewardFive");
+  const rewardContract5 = await rewardFactory5.deploy();
+  await rewardContract5.deployed();
+  console.log(`contract address : ${rewardContract5.address}`);
 }
 
 main().catch((error) => {
