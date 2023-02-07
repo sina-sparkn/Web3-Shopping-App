fad3b890825107430cd8dc43a3ea0349570ebd6d
ssparknt <sina.hakim.red@gmail.com>
2023-02-07 02:24:17 +0200

    start page designing (#2)

--- frontEnd/src/components/Start.jsx
+++ frontEnd/src/components/Start.jsx
@@ -1,53 +1,121 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import wallets from "../assets/image/start/wallets.png";
+import click from "../assets/image/start/click.png";
+import { Link } from "react-router-dom";
 
 const Start = () => {
   return (
-    <main className="h-full p-5 flex flex-col items-center gap-7 bg-gradient-to-b from-maindarkpurple/20 to-maindarkpurple">
+    <main className="p-5 md:p-20 flex flex-col items-center gap-7 bg-gradient-to-b from-maindarkpurple/20 to-maindarkpurple">
       <h2 className="w-full text-xl flex justify-start items-center gap-2 font-semibold">
         MOONINK Features
         <FontAwesomeIcon className="text-lg" icon={faArrowRight} />
       </h2>
-      <section className="grid lg:grid-cols-2 gap-5 w-full">
-        <div className="h-52 flex flex-col gap-14 ring-2 ring-violet-400/30 p-5 bg-violet-500/5 relative overflow-hidden rounded-lg ">
-          <div className="flex z-30 flex-col gap-2">
-            <p className="font-extrabold text-3xl relative ">No Email needed</p>
-            <p className="text-white/80 text-lg relative">
-              Just use your Preferred Wallet
+      <hr className="w-full border-0 h-0.5 bg-violet-500/20" />
+      <div className="flex flex-col gap-16 w-full">
+        <section className="flex flex-col gap-5 sm:flex-row">
+          <div className="w-full flex flex-col gap-3">
+            <p className="text-3xl font-semibold">No Signup Email needed</p>
+            <p className="text-white/70 text-lg">
+              Just use your Preferred Wallet and thats pretty much it.
             </p>
           </div>
-          <a
-            target="_blank"
-            href="https://explorer.walletconnect.com/?type=wallet"
-            className="flex items-center p-2 z-30 bg-violet-500/50 hover:ring-2 hover:ring-violet-500/70 max-w-max rounded-lg duration-300"
-          >
-            Explore all
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-arrow-up-right"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+          <div className="h-52 w-full flex flex-col gap-14 ring-2 ring-violet-400/30 p-5 bg-violet-500/5 relative overflow-hidden rounded-xl">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center z-30">
+                <span className="text-4xl font-extrabold">170</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-plus"
+                  width="28"
+                  height="28"
+                  viewBox="0 0 24 24"
+                  strokeWidth="4"
+                  stroke="currentColor"
+                  fill="none"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M12 5l0 14"></path>
+                  <path d="M5 12l14 0"></path>
+                </svg>
+              </div>
+              <p className="text-white/60">Wallets Supported</p>
+            </div>
+            <a
+              target="_blank"
+              href="https://explorer.walletconnect.com/?type=wallet"
+              className="flex items-center p-2 z-30 bg-violet-500/50 hover:ring-2 hover:ring-white max-w-max rounded-lg duration-300"
             >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M17 7l-10 10"></path>
-              <path d="M8 7l9 0l0 9"></path>
-            </svg>
-          </a>
-          <img
-            src={wallets}
-            alt="wallets"
-            className="absolute z-0 top-0 -right-16 sm:right-0 h-full"
-          />
-        </div>
-        {/* <div className="bg-red-500 px-5 py-10">Easy to use</div> */}
-      </section>
+              Explore all
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-arrow-up-right"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M17 7l-10 10"></path>
+                <path d="M8 7l9 0l0 9"></path>
+              </svg>
+            </a>
+            <img
+              src={wallets}
+              alt="wallets"
+              className="absolute z-0 top-0 scale-125 right-0 h-full"
+            />
+          </div>
+        </section>
+        <section className="flex flex-col gap-5 sm:flex-row">
+          <div className="w-full flex flex-col gap-3">
+            <p className="text-3xl font-semibold">
+              Easiest Shopping Experience
+            </p>
+            <p className="text-white/70 text-lg">
+              It can't be easir you can purchse your orders with just one click.
+              no Credit or debit card needed. $MINK token is all it takes.
+            </p>
+          </div>
+          <div className="h-52 w-full flex flex-col gap-14 ring-2 ring-violet-400/30 p-5 bg-violet-500/5 relative overflow-hidden rounded-xl ">
+            <div className="flex flex-col gap-2">
+              <p className="text-3xl font-bold">it's done.</p>
+              <p className="text-white/70 text-lg z-30">Just One Click.</p>
+            </div>
+
+            <Link to="Tshirts" className="max-w-max">
+              <button className="flex items-center p-2 z-30 bg-violet-500 hover:ring-2 hover:ring-white max-w-max rounded-lg duration-300">
+                Get Started
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-arrow-up-right"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M17 7l-10 10"></path>
+                  <path d="M8 7l9 0l0 9"></path>
+                </svg>
+              </button>
+            </Link>
+            <img
+              src={click}
+              alt="wallets"
+              className="absolute z-0 top-0 right-0 h-full"
+            />
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
